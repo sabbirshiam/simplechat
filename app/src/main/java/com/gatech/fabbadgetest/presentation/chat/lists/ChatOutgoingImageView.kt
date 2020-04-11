8a8016bf538673665c8c0aed250e8dc07ce109c9
sabbirshiam <sabbirshiam@gmail.com>
2020-04-11 19:59:32 +0900

    android material ShapableImageview testing.

--- app/src/main/java/com/gatech/fabbadgetest/presentation/chat/lists/ChatOutgoingImageView.kt
+++ app/src/main/java/com/gatech/fabbadgetest/presentation/chat/lists/ChatOutgoingImageView.kt
@@ -1,22 +1,14 @@
 package com.gatech.fabbadgetest.presentation.chat.lists
 
 import android.content.Context
-import android.graphics.drawable.Drawable
-import android.net.Uri
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
 import com.gatech.fabbadgetest.R
 import com.gatech.fabbadgetest.domain.models.ChatMessageModel
+import com.google.android.material.shape.CornerFamily
 import kotlinx.android.synthetic.main.chat_outgoing_img_view.view.*
 import timber.log.Timber
 
@@ -42,11 +34,16 @@ class ChatOutgoingImageView : ConstraintLayout {
         var landscapeUrl = "https://vastphotos.com/files/uploads/photos/10592/lake-at-twilight-l.jpg"
         var portraitUrl = "https://images.squarespace-cdn.com/content/v1/524356bae4b04817bad65d5c/1555354881905-G6X8W8B0SZY2PFQJ1LTL/ke17ZwdGBToddI8pDm48kA-E3A5nZWA4inW_WqcGKdwUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8PaoYXhp6HxIwZIk7-Mi3Tsic-L2IOPH3Dwrhl-Ne3Z2Qs5oYuN4O0sxBD_kPJGPstUprkcqQ60xRmZ9Cmzp5ccKMshLAGzx4R3EDFOm1kBS/casey_baugh_studio_painting.png"
 
+        val radius = resources.getDimension(R.dimen.default_corner_radius)
+        outgoingImage.shapeAppearanceModel = outgoingImage.shapeAppearanceModel
+            .toBuilder()
+            .setTopRightCorner(CornerFamily.ROUNDED, radius)
+            .setTopLeftCorner(CornerFamily.ROUNDED, radius)
+            .setBottomLeftCorner(CornerFamily.ROUNDED, radius)
+            .build()
         Timber.d("size:: ${Target.SIZE_ORIGINAL}")
         Glide.with(this)
             .load(portraitUrl)
-            .fitCenter()
-            .apply(RequestOptions().transform(RoundedCorners(50)))
             .into(outgoingImage)
 
 
