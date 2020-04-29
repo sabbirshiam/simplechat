a2ca65c0658c02c22dca00513dd4b67de5c74097
sabbirshiam <sabbirshiam@gmail.com>
2020-04-29 20:14:49 +0900

    added link text wire.

--- app/src/main/java/com/gatech/fabbadgetest/presentation/chat/lists/ChatIncomingTextView.kt
+++ app/src/main/java/com/gatech/fabbadgetest/presentation/chat/lists/ChatIncomingTextView.kt
@@ -1,9 +1,12 @@
 package com.gatech.fabbadgetest.presentation.chat.lists
 
 import android.content.Context
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.text.util.LinkifyCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions.circleCropTransform
 import com.gatech.fabbadgetest.R
@@ -26,7 +29,11 @@ class ChatIncomingTextView : ConstraintLayout {
     }
 
     fun onBindData(data: ChatMessageModel) {
-        message_body.text = data.message
+        if (data.message.contains("https")) {
+            message_body.movementMethod = LinkMovementMethod.getInstance()
+            message_body.text = data.message
+            LinkifyCompat.addLinks(message_body, Linkify.ALL)
+        }
 
         Glide.with(this)
             .load(URLDecoder.decode("https://pbs.twimg.com/profile_images/578558726971371521/TEZwnCCV_400x400.jpeg", "UTF-8"))
